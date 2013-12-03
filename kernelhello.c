0f3b0c6e38b8a13b450ad25ced5f25f054a85422
Fredrik Simonsson <simson@thesimson.net>
2013-12-03 21:36:17 +0100

    Proc create changes in kernel 3.10

--- kernelhello.c
+++ kernelhello.c
@@ -1,9 +1,14 @@
 // simple example of a kernel hello world.
 // As found on http://www.tldp.org/LDP/lkmpg/2.6/html/x121.html
 
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
-#include <linux/proc_fs.h>	/* Necessary because we use the proc fs */
+/* Needed by all modules */
+#include <linux/module.h>
+
+/* Needed for KERN_INFO */
+#include <linux/kernel.h>
+
+/* Necessary because we use the proc fs */
+#include <linux/proc_fs.h>
 
 MODULE_LICENSE("GPL");
 
@@ -12,15 +17,23 @@ MODULE_LICENSE("GPL");
 int procfile_read(char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data);
 struct proc_dir_entry *Our_Proc_File;
 
+static const struct file_operations proc_file_fops = {
+ .owner = THIS_MODULE,
+ .open  = open_callback,
+ .read  = procfile_read,
+};
+
+
+
 int kernelhello; // Store a global in the kernel
 int init_module(void)
 {
 	printk(KERN_INFO "Hello world 1.\n");
 
-	Our_Proc_File = create_proc_entry(procfs_name, 0644, NULL);
+	Our_Proc_File = proc_create(procfs_name, 0644, NULL);
 
 	if (Our_Proc_File == NULL) {
-		remove_proc_entry(procfs_name, NULL);
+	   remove_proc_entry(procfs_name, NULL);
 		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n",
 				procfs_name);
 		return -ENOMEM;
